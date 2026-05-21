8c908e916e252450c285d0df544c8cbcd6c5c5fe
AstralDesigns <ianmiruka1@gmail.com>
2026-05-21 23:47:07 +0300

    Clean logout, reboot and shutdown via hyprshutdown

--- .config/hypr/scripts/power.sh
+++ .config/hypr/scripts/power.sh
@@ -7,42 +7,9 @@
 
 USERNAME=$(whoami)
 
-terminate_clients() {
-  TIMEOUT=5
-  # Get a list of all client PIDs in the current Hyprland session
-  client_pids=$(hyprctl clients -j | jq -r '.[] | .pid')
-
-  # Send SIGTERM (kill -15) to each client PID and wait for termination
-  for pid in $client_pids; do
-    echo ":: Sending SIGTERM to PID $pid"
-    kill -15 $pid
-  done
-
-  start_time=$(date +%s)
-  for pid in $client_pids; do
-    # Wait for the process to terminate
-    while kill -0 $pid 2>/dev/null; do
-      current_time=$(date +%s)
-      elapsed_time=$((current_time - start_time))
-
-      if [ $elapsed_time -ge $TIMEOUT ]; then
-        echo ":: Timeout reached."
-        return 0
-      fi
-
-      echo ":: Waiting for PID $pid to terminate..."
-      sleep 1
-    done
-
-    echo ":: PID $pid has terminated."
-  done
-}
-
 if [[ "$1" == "exit" ]]; then
   echo ":: Exit"
-  terminate_clients
-  sleep 2
-  hyprctl dispatch 'hl.dsp.exit()'
+  hyprshutdown > /dev/null 2>&1 -t 'Logging out...' --post-cmd "hyprctl dispatch hl.dsp.exit()"
 fi
 
 if [[ "$1" == "lock" ]]; then
@@ -52,16 +19,12 @@ fi
 
 if [[ "$1" == "reboot" ]]; then
   echo ":: Reboot"
-  terminate_clients
-  sleep 2
-  systemctl reboot
+  hyprshutdown > /dev/null 2>&1 -t 'Restarting...' --post-cmd 'reboot'
 fi
 
 if [[ "$1" == "shutdown" ]]; then
   echo ":: Shutdown"
-  terminate_clients
-  sleep 2
-  systemctl poweroff
+  hyprshutdown > /dev/null 2>&1 -t 'Shutting down...' --post-cmd 'shutdown -P 0'
 fi
 
 if [[ "$1" == "suspend" ]]; then
